1e75adb8aa7cf68df447d8d7d0de0c859dad1bc3
ProfutW <1203389799@qq.com>
2018-06-27 21:01:18 +0800

    feat: fix some errors in video player

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
 const views = require('koa-views');
+const static = require('koa-static');
 const {
     resolve
 } = require('path');
@@ -10,12 +11,11 @@ app.use(views(resolve(__dirname, './views'), {
     extension: 'pug',
 }));
 
+app.use(static(__dirname + '/public'));
+
 app.use(async (ctx, next) => {
     await next();
-    await ctx.render('index', {
-        you: 'Zhen',
-        me: 'Wei',
-    });
+    await ctx.render('index');
 });
 
 app.listen(1337);
